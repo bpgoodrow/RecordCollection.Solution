eb9fbef2a1ea09456d362ee42db6a916c00055f6
Ben Goodrow <bpgoodrow@gmail.com>
2022-07-21 11:08:51 -0700

    Basic functionality complete

--- RecordCollection/Models/Record.cs
+++ RecordCollection/Models/Record.cs
@@ -1,21 +1,21 @@
 using System.Collections.Generic;
 
-namespace ToDoList.Models
+namespace RecordCollection.Models
 {
-  public class Item
+  public class Record
   {
     public string Description { get; set; }
     public int Id { get; }
-    private static List<Item> _instances = new List<Item> { };
+    private static List<Record> _instances = new List<Record> { };
 
-    public Item(string description)
+    public Record(string description)
     {
       Description = description;
       _instances.Add(this);
       Id = _instances.Count;
     }
 
-    public static List<Item> GetAll()
+    public static List<Record> GetAll()
     {
       return _instances;
     }
@@ -24,7 +24,7 @@ namespace ToDoList.Models
     {
       _instances.Clear();
     }
-    public static Item Find(int searchId)
+    public static Record Find(int searchId)
     {
       return _instances[searchId-1];
     }
